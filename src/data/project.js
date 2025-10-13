773337e52ed28c850c5fc99294f577491c9ba96f
m0osshu <dasalazarberrios27@gmail.com>
2025-10-13 12:37:30 -0300

    Subiendo proyecto al nuevo repositorio portafolio

--- src/data/project.js
+++ src/data/project.js
@@ -1,6 +1,7 @@
 import apiRestImg from '../assets/img/projects/kmDigital.png';
 import appMovilImg from '../assets/img/projects/kozi.png';
 import paginaWebImg from '../assets/img/projects/logoMama.png';
+import paginaWeBImg from '../assets/img/home/a10.jpg';
 
 const projects = [
     {
@@ -9,23 +10,31 @@ const projects = [
       description: 'Proyecto desarrollado con AndroidStudio y Kotlin',
       tecnologies: ['Kotlin', 'AndroidStudio'],
       image: apiRestImg,
-      url: 'https://github.com/Dani-txt/Xpress.git'
+      url: 'https://github.com/m0osshu/KMdigital.git'
     },
     {
       id: 2,
-      name: 'Proyecto Aplicación movil',
-      description: 'Proyecto desarrollado con AndroidStudio y Kotlin',
+      name: 'Kozi',
+      description: 'Proyecto desarrollado con Html, CSS, JavaScript y Bootstrap.',
       tecnologies: ['JavaScript', 'HTML', 'CSS'],
       image:  appMovilImg,
-      url: 'https://github.com/Dani-txt/TuAuto'
+      url: 'https://github.com/m0osshu/kozi.git'
     },
     {
       id: 3,
-      name: 'Proyecto Página web',
-      description: 'Proyecto desarrollado con React, JavaScript y Bootstrap.',
+      name: 'PetShop',
+      description: 'Proyecto desarrollado con JavaScript y Bootstrap.',
       tecnologies: ['JavaScript', 'HTML', 'CSS',],
       image:  paginaWebImg,
-      url: 'https://github.com/Dani-txt/Libreria.git'
+      url: 'https://github.com/m0osshu/PetShop.git'
+    },
+        {
+      id: 4,
+      name: 'Portafolio',
+      description: 'Proyecto desarrollado con JavaScript y React',
+      tecnologies: ['JavaScript', 'CSS', 'React'],
+      image:  paginaWeBImg,
+      url: 'https://github.com/m0osshu/PortafolioMoshu.git'
     },
   ];
   
